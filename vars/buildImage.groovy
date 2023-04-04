0c0a9449cb08b0046a059cf6b11d784e31aa1243
Pramod Choyal <choyalpramod@gmail.com>
2023-04-04 16:15:51 +0200

    testing

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,17 @@
 #!/usr/bin/env groovy
 
-import com.example.Docker
+// import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+// def call(String imageName) {
+//     return new Docker(this).buildDockerImage(imageName)
+// }
+
+def call() {
+    echo "building the docker image..."
+    echo "${params.VERSION}"
+    withCredentials([usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh "docker build -t choyalpramod/demo-app:jma-${params.VERSION} ."
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "docker push choyalpramod/demo-app:jma-${params.VERSION}"
+    }
 }
