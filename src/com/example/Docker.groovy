6da1f9bba0affb37690e65e90f4ce6b0828590ab
Pramod Choyal <choyalpramod@gmail.com>
2023-04-05 12:11:21 +0200

    testing

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -8,14 +8,14 @@ class Docker implements Serializable {
     Docker(script) {
         this.script = script
     }
-
+ 
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
         script.sh "docker build -t $imageName ."
     }
 
     def dockerLogin() {
-        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
             script.sh "echo $script.PASS | docker login -u $script.USER --password-stdin"
         }
     }
